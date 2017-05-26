2cbb5400bfde1455a2978871286814ee6eee0386
SMVictor <victor.salvatierra@outlook.com>
2017-05-26 00:07:23 -0600

    Master page con template y recursos necesarios para las pantallas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,140 @@
+@extends('masterpage.app')
+
+@section('body')
+<main id="main">
+    <section class="content-block bg-white">
+<div class="container">
+<header class="content-heading">
+<h2 class="main-heading">THE ADVENTURE</h2>
+<span class="main-subtitle">Mastering the art of perfect adventure for 10+ years in the wild.</span>
+<div class="seperator"></div>
+</header>
+<div class="adventure-holder gallery-home-holder">
+<div class="row">
+<div class="col-md-6 img-block">
+ 
+<ul class="gallery-list gallery-with-icon">
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-01.jpg.pagespeed.ic.HtOxMtAC7I.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-bird"></span>
+<span class="info">aerial</span>
+</a>
+</li>
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-02.jpg.pagespeed.ic.VbopAx93vf.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-peak"></span>
+<span class="info">land</span>
+</a>
+</li>
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-03.jpg.pagespeed.ic.jOTgZn5gbA.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-water"></span>
+<span class="info">water</span>
+</a>
+</li>
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-04.jpg.pagespeed.ic.OKfMVx7KGD.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-asia"></span>
+<span class="info">asia</span>
+</a>
+</li>
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-05.jpg.pagespeed.ic.26GCbol3Ir.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-arctic"></span>
+<span class="info">arctic</span>
+</a>
+</li>
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-06.jpg.pagespeed.ic.DTHFnERJx7.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-africa"></span>
+<span class="info">africa</span>
+</a>
+</li>
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-07.jpg.pagespeed.ic.1xDORmdhbh.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-plant"></span>
+<span class="info">nature</span>
+</a>
+</li>
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-08.jpg.pagespeed.ic.gkWMlwGw0o.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-wildlife"></span>
+<span class="info">wildness</span>
+</a>
+</li>
+<li>
+<a href="#" class="thumbnail">
+<img src="img/gallery/ximg-09.jpg.pagespeed.ic.MTPXu-AkHH.jpg" height="165" width="170" alt="image description">
+<span class="hover icon-diamond"></span>
+<span class="info">all</span>
+</a>
+</li>
+</ul>
+</div>
+<div class="col-md-6 text-block">
+<div class="centered">
+<h2 class="intro-heading">Living a Life of Adventure</h2>
+<p class="intro">This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. </p>
+<a href="#" class="btn btn-info-sub btn-md btn-shadow radius">Dig More</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<video width="320" height="240" controls>
+  <source src="videos/Rammstein.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+ 
+<div class="testimonial-holder parallax" data-stellar-background-ratio="0.25" id="testimonial-home-page">
+<div class="container">
+<div id="testimonial-home-slide">
+<div class="slide">
+<blockquote class="testimonial-quote">
+<div class="img">
+<img src="img/thumbs/ximg-01.jpg.pagespeed.ic.msLTVDOfeL.jpg" height="112" width="112" alt="image description">
+</div>
+<div class="text">
+<cite>STEVE JORGAN - NORWAY</cite>
+<q>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.”</q>
+</div>
+</blockquote>
+</div>
+<div class="slide">
+<blockquote class="testimonial-quote">
+<div class="img">
+<img src="img/thumbs/ximg-02.jpg.pagespeed.ic.zCcQQH8dHj.jpg" height="112" width="112" alt="image description">
+</div>
+<div class="text">
+<cite>DARREN SPRATT - AUSTRALIA</cite>
+<q>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.”</q>
+</div>
+</blockquote>
+</div>
+<div class="slide">
+<blockquote class="testimonial-quote">
+<div class="img">
+<img src="img/thumbs/ximg-03.jpg.pagespeed.ic.USLZpY_r8N.jpg" height="112" width="112" alt="image description">
+</div>
+<div class="text">
+<cite>RIYA YAHIDA - SPAIN</cite>
+<q>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.”</q>
+</div>
+</blockquote>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
