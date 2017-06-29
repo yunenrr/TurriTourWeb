3695a61718c7d34dc2f4bb71b716d9d6024654de
SMVictor <victor.salvatierra@outlook.com>
2017-06-28 20:40:13 -0600

    Con Bayes sin pero sin usar

--- app/Node.php
+++ app/Node.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Node extends Model
 {
     protected $table = 'nodes'; //Tabla a la que se relaciona
-    protected $fillable = ['latitude','longitude','idcategories', 'idtypeactivity','value','name','information','slogan','pathlogo','pathvideoimage','urlfacebook','urlweb'];
+    protected $fillable = ['latitude','longitude','idcategories', 'idtypeactivity','value','name','information','slogan','pathlogo','pathvideoimage','urlfacebook','urlweb', 'class'];
     protected $guarded = 'idnodes';
 }//Fin de la clase
